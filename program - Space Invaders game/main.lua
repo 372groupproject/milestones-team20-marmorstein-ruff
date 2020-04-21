9598fed8c67424bda8bee91bbacf360e846bb748
tnruff <46604112+tnruff@users.noreply.github.com>
2020-04-20 23:36:30 -0700

    Update main.lua

--- program - Space Invaders game/main.lua
+++ program - Space Invaders game/main.lua
@@ -1,4 +1,3 @@
-
 function love.update()
   --updates the game's frame-by-frame state
 end
@@ -6,4 +5,10 @@ end
 function love.draw()
   --draws the game state to the screen
   love.graphics.ellipse("fill", love.mouse.getX(), 500, 50, 50)
+end
+
+function love.mousepressed(x,y,button)
+  if button == 1 then
+    love.graphics.ellipse("fill",x,y,5,5)
   end
+end
