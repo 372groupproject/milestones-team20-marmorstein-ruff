f5f51f7fa2d826407b8db759cd98420eea4c6a0e
Jade <jhmarmo@charter.net>
2020-04-29 21:30:39 -0700

    got functional enemies, collisions and restart

--- program - Space Invaders game/main.lua
+++ program - Space Invaders game/main.lua
@@ -1,15 +1,24 @@
+--collision detection function, returns true if objects are overlapping
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
+
 -- create objects with tables
 -- timer for enemy spawning
 enemyTimerMax = 0.4
-enemyTimer = enemyTimerMax
-
--- enemyimg
-
+enemyTimer = enemyTimerMax 
+enemyimg = nil
 enemies = {}
-
+playerAlive = true
+player = nil
 
 function love.load()
   bg = love.graphics.newImage("space bg.png")
+  enemyimg = love.graphics.newImage("enemy.png")
+  player = love.graphics.newImage("spaceship.png")
 end
 
 function love.update(dt)
@@ -24,7 +33,7 @@ function love.update(dt)
   if enemyTimer < 0 then 
     enemyTimer = enemyTimerMax
     
-    newEnemy = {x = (math.random(10, love.graphics.getWidth() - 10)), y = -10} --img = enemyimg
+    newEnemy = {x = (math.random(10, love.graphics.getWidth() - 10)), y = -10, img = enemyimg}
     table.insert(enemies, newEnemy)
   end
   
@@ -38,6 +47,25 @@ function love.update(dt)
     end 
   
   end
+
+  --collision detection
+  for i, enemy in ipairs(enemies) do
+    if(CheckCollision(enemy.x, enemy.y, enemyimg:getWidth(), enemyimg:getHeight(), 
+        love.mouse.getX(), 500, player:getWidth(), player:getHeight()) and playerAlive) then
+        table.remove(enemies,i)
+        playerAlive = false
+    end
+  end
+  
+  --handle restarting the game when R is pressed
+  if not playerAlive and love.keyboard.isDown('r') then
+    --remove all bulets and enemies
+    enemies = {}
+    --reset timer
+    enemyTimer = enemyTimerMax
+    
+    playerAlive = true
+  end
 end
 
 function love.draw(dt)
@@ -45,7 +73,16 @@ function love.draw(dt)
   --background
   love.graphics.draw(bg, 0, 0, 0, 2, 2)
   --player
-  love.graphics.ellipse("fill", love.mouse.getX(), 500, 50, 50)
+  if playerAlive then
+    love.graphics.draw(player, love.mouse.getX(), 500)
+  else 
+    love.graphics.print("You died, press 'R' to restart", love.graphics:getWidth()/2-75, love.graphics:getHeight()/2-10)
+  end
+  --enemies
+  for i, enemy in ipairs(enemies) do
+    love.graphics.draw(enemy.img, enemy.x, enemy.y)
+  end
+  
 end
 
 function love.mousepressed(x,y,button)
